787f75532ad6b64bf959bd60382fbb22f512270a
HMichaut <hectormichaut@gmail.com>
2021-09-09 00:10:06 +0200

    omniauth config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,5 +309,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :github, "APP_ID", "APP_SECRET"
+  config.omniauth :github, "8fa704abb59e1c7b03f2", "86010bf58da889fe53f65a730ebd35e6e6a8fea8"
 end

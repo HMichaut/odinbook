94836a53762f233490be492b1181500cee313cdc
HMichaut <hectormichaut@gmail.com>
2021-09-09 23:12:20 +0200

    omniauth config

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_account_update_params, only: [:update]
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[email image])
   end
 end
